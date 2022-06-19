f7cb117b40897a52e62daa11ddfb66b647e2c387
Patrick Roberts <git@patrickroberts.dev>
2022-06-19 14:49:06 -0500

    Integrated socket.io using custom next server

---
+++ server.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http');
+/** @type {typeof import('next').default} */
+const next = require('next');
+const { Server } = require('socket.io');
+const { loadEnvConfig } = require('@next/env');
+
+const dev = process.env.NODE_ENV !== 'production';
+const env = loadEnvConfig(process.cwd(), dev).combinedEnv;
+const hostname = env.HOSTNAME;
+const port = env.PORT;
+
+const app = next({ dev, hostname, port });
+const requestHandler = app.getRequestHandler();
+const httpServer = createServer(requestHandler);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+
+});
+
+app.prepare().then(() => {
+  httpServer.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+});
